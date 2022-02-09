db83177768f1c582fa0087bcbe8ac33eacc8960c
ymcla <ymclapp@yahoo.com>
2022-02-09 11:25:18 -0600

    added more base files and server is running

--- server.js
+++ server.js
@@ -1,2 +1,32 @@
 'use strict';
 
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const pg = require('pg');
+//const db = require('db');
+
+//routes
+app.get('/', (request, response) => {
+    response.send('Home Page!');
+});
+
+app.get('*'), (request, response) =>
+  response.status(404).send('This route does not exist');
+
+
+
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(`App is listening on http://localhost:${PORT}`));
+
+
+
